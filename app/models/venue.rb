4915623e9a5b4f1e1ee783e5d0b71ca20d804c7d
Raghu Betina <raghu@firstdraft.com>
2019-12-05 04:10:17 +0000

    Direct association between venues and photos

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,9 @@
 class Venue < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :dependent => :destroy
+
   has_many   :users,
              :dependent => :nullify
 
--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Venue, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:photos) }
+
     it { should have_many(:users) }
 
     end
