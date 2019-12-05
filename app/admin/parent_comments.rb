c20937eb9f29a2eec2754bb21f790dec815bdecc
Raghu Betina <raghu@firstdraft.com>
2019-12-05 04:10:44 +0000

    Whitelist columns in ParentComment for ActiveAdmin

--- app/admin/parent_comments.rb
+++ app/admin/parent_comments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register ParentComment do
+
+ permit_params :user_id, :body
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
