65b03c992d7e15aaa221f1d1263b7c7289d058df
Raghu Betina <raghu@firstdraft.com>
2019-12-05 04:10:57 +0000

    Only direct_message user can modify direct_message

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -1,4 +1,14 @@
 class DirectMessagesController < ApplicationController
+  before_action :current_user_must_be_direct_message_sender, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_direct_message_sender
+    direct_message = DirectMessage.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == direct_message.sender
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @direct_messages = DirectMessage.all
 
