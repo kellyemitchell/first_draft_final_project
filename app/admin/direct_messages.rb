810c87cb18652730d06d36c6a01f30180be735e8
Raghu Betina <raghu@firstdraft.com>
2019-12-05 04:10:47 +0000

    Whitelist columns in DirectMessage for ActiveAdmin

--- app/admin/direct_messages.rb
+++ app/admin/direct_messages.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register DirectMessage do
+
+ permit_params :sender_id, :parent_id, :body
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
